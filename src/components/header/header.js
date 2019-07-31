61e85ff10ebf025cf68bf578e89083e03fb582df
Dmytro Frolov1 <Dmytro_Frolov1@epam.com>
2019-07-31 10:44:12 +0300

    dist deploy

--- src/components/header/header.js
+++
@@ -1,8 +0,0 @@
-import "./header.sass";
-
-import "owl.carousel2/dist/assets/owl.carousel.css";
-import "owl.carousel2";
-import "../../../node_modules/video.js/dist/video-js.min.css"
-import "videojs-youtube";
-
-import "./../../../assets/img/logo/logo1.png"
