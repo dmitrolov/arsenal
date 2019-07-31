af9b5174d8ea7fe371bb6e02b0998ed37fa0a427
Dmytro Frolov1 <Dmytro_Frolov1@epam.com>
2019-07-31 15:43:30 +0300

    src sets added, code improvements

--- src/components/bar-menu/bar-menu.js
+++ src/components/bar-menu/bar-menu.js
@@ -1,15 +1,24 @@
 import $ from "jquery"
 import './bar-menu.sass'
 
-import './../../../assets/img/bar-menu/page1.webp'
-import './../../../assets/img/bar-menu/page2.webp'
-import './../../../assets/img/bar-menu/page3.webp'
-import './../../../assets/img/bar-menu/page4.webp'
-import './../../../assets/img/bar-menu/page5.webp'
-import './../../../assets/img/bar-menu/page6.webp'
-import './../../../assets/img/bar-menu/page7.webp'
-import './../../../assets/img/bar-menu/page8.webp'
-import './../../../assets/img/bar-menu/page9.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page1.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page1.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page2.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page2.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page3.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page3.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page4.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page4.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page5.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page5.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page6.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page6.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page7.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page7.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page8.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page8.webp'
+import './../../../assets/img/bar-menu/bar-menu-320/page9.webp'
+import './../../../assets/img/bar-menu/bar-menu-760/page9.webp'
 
 
 $('.bar-menu-carousel').owlCarousel({
--- src/components/photo-gallery/photo-gallery.js
+++ src/components/photo-gallery/photo-gallery.js
@@ -1,16 +1,49 @@
 import "./photo-gallery.sass"
 
-import "../../../assets/img/photo-gallery/about1.webp"
-import "../../../assets/img/photo-gallery/about2.webp"
-import "../../../assets/img/photo-gallery/about3.webp"
-import "../../../assets/img/photo-gallery/about4.webp"
-import "../../../assets/img/photo-gallery/about5.webp"
-import "../../../assets/img/photo-gallery/about6.webp"
-import "../../../assets/img/photo-gallery/about7.webp"
-import "../../../assets/img/photo-gallery/about8.webp"
-import "../../../assets/img/photo-gallery/about9.webp"
-import "../../../assets/img/photo-gallery/about10.webp"
-import "../../../assets/img/photo-gallery/about11.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-100/about1.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about1.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about1.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about2.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about2.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about2.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about3.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about3.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about3.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about4.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about4.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about4.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about5.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about5.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about5.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about6.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about6.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about6.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about7.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about7.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about7.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about8.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about8.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about8.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about9.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about9.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about9.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about10.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about10.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about10.webp"
+
+import "../../../assets/img/photo-gallery/photo-gallery-100/about11.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-320/about11.webp"
+import "../../../assets/img/photo-gallery/photo-gallery-767/about11.webp"
+
 
 $('.photo-gallery-carousel').owlCarousel({
     loop:true,
